ba418df636949dfd88827c34c5cc967a7266c913
samyadel <samy.adel@gembitz.com>
2020-08-26 17:31:04 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Generate a random target number
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+// Get the difference between a number and a target
+const getAbsoluteDistance = (num, target) => Math.abs(target - num);
+
+// Compare the user guess offset to the computer guess offset
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+  let userGuessDiff = getAbsoluteDistance(userGuess, targetNumber);
+  let computerGuessDiff = getAbsoluteDistance(computerGuess, targetNumber);
+
+  if (userGuess < 0 || userGuess > 9) {
+    alert("Your number is out of range");
+    return;
+  }
+
+  console.log(`User Guess Difference: ${userGuessDiff}`);
+  console.log(`Computer Guess Difference: ${computerGuessDiff}`);
+  console.log(userGuessDiff <= computerGuessDiff);
+
+  return userGuessDiff <= computerGuessDiff;
+};
+
+// Update the winner's score
+const updateScore = (winner) => {
+  winner === "human" ? humanScore++ : computerScore++;
+};
+
+// Update the round number
+const advanceRound = () => {
+  currentRoundNumber++;
+};
